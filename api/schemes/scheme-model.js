6ad4980c3d5b5d5653ac8a4d4c83896fd2a732f3
Mychael M <mychaelbmenges@gmail.com>
2021-01-05 18:39:35 -0700

    added helper funtion findSteps

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -10,7 +10,16 @@ function findById(id) {
     .first()
 }
 
+function findSteps(id){
+  return db("schemes")
+    .innerJoin("steps", "schemes.id", "steps.scheme_id")
+    .where("schemes.id", id)
+    .select("steps.id", "schemes.scheme_name", "steps.step_number", "steps.instructions")
+    .orderBy("steps.step_number")
+}
+
 module.exports = {
   find,
   findById,
+  findSteps,
 }
